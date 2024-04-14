dc333c4a78507008765bb8225fa9355fc5233d8b
vnr235 <vnr235@gmail.com>
2024-04-14 08:25:51 +0530

    added database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-import express from  'express';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app= express();
+mongoose.connect(process.env.Mongo)
+    .then(() => {
+        console.log('connected to mongodb');
+    })
+    .catch(err => { console.error(err) });
+const app = express();
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log(`server is runing on port 3000`)
 });
